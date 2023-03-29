a2855edb5dba6aeeac7ff2f90e20d8bf4ecce9f2
양정훈 <yjhoon3658@naver.com>
2023-03-29 16:37:17 +0900

    [BOJ] 14621_나만 안되는 연애

--- WEEK8/BOJ_14621/jeonghoon.java
+++ WEEK8/BOJ_14621/jeonghoon.java
@@ -1,177 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    public enum DIRECTION {
-        ZERO, UP, DOWN, LEFT, RIGHT
-    }
+public class BOJ_14621 {
 
-    static Smell[][] board;
-    static int size;
-    static int sharkNum;
-    static int smellTime;
-    static DIRECTION[][][] movePriority;
-    static Shark[] sharks;
-    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static int remainShark;
+    // prim 알고리즘에 사용할 graph
+    static List<List<Pair>> graph = new ArrayList<>();
+    // selected로 연결 여부 확인
+    static boolean[] selected;
+    // schoolType의 true/false값에 따라 남초학교, 여초학교 구분
+    static boolean[] schoolType;
+    static int schoolNum;
 
-    static class Smell {
-        int owner;
-        int life;
+    static class Pair {
+        int first;
+        int second;
 
-        public Smell(int owner, int life) {
-            this.owner = owner;
-            this.life = life;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
         }
     }
 
-    static class Shark {
-        boolean state;
-        DIRECTION curDir;
-        int curPosY;
-        int curPosX;
-
-        public Shark(boolean state, DIRECTION curDir, int curPosY, int curPosX) {
-            this.state = state;
-            this.curDir = curDir;
-            this.curPosY = curPosY;
-            this.curPosX = curPosX;
-        }
-    }
-
-    public static DIRECTION numToDir(int num) {
-        switch (num) {
-            case 1:
-                return DIRECTION.UP;
-            case 2:
-                return DIRECTION.DOWN;
-            case 3:
-                return DIRECTION.LEFT;
-            case 4:
-                return DIRECTION.RIGHT;
-        }
-        return DIRECTION.ZERO;
-    }
-
-    public static boolean check(int row, int col) {
-        return row >= 0 && row < size && col >= 0 && col < size;
-    }
-
-    public static void move() {
-        for (int i = 1; i <= sharkNum; i++) {
-            if (!sharks[i].state) continue;
-            boolean moveSuccess = false;
-            for (int j = 0; j < 4; j++) {
-                int nextRow = sharks[i].curPosY + dir[movePriority[i][sharks[i].curDir.ordinal() - 1][j].ordinal() - 1][0];
-                int nextCol = sharks[i].curPosX + dir[movePriority[i][sharks[i].curDir.ordinal() - 1][j].ordinal() - 1][1];
-                if (check(nextRow, nextCol) && board[nextRow][nextCol].owner == 0) {
-                    sharks[i].curPosY = nextRow;
-                    sharks[i].curPosX = nextCol;
-                    sharks[i].curDir = movePriority[i][sharks[i].curDir.ordinal() - 1][j];
-                    moveSuccess = true;
+    static int prim() {
+        int result = 0;
+        // 가중치 기준으로 오름차순 정렬되는 PriorityQueue 선언
+        Queue<Pair> dist = new PriorityQueue<>((o1, o2) -> o1.second - o2.second);
+        // 초기 한개의 노드를 우선 Queue에 add
+        dist.add(new Pair(1, 0));
+        // 모든 노드가 연결되어야 하므로 schoolNum 만큼 반복
+        for (int i = 1; i <= schoolNum; i++) {
+            int cur = -1;
+            int minDist = Integer.MAX_VALUE;
+            while(!dist.isEmpty()) {
+                Pair tmp = dist.poll();
+                // Queue에서 꺼낸 값이 방문하지 않은 노드라면
+                cur = tmp.first;
+                // PQ에 의해 정렬되어 있으므로 현재 poll된 가중치가 그래프와 연결할 수 있는 가장 작은 노드 + 가중치
+                if (!selected[cur]) {
+                    minDist = tmp.second;
                     break;
                 }
             }
-            if (sharks[i].state && !moveSuccess) {
-                for (int j = 0; j < 4; j++) {
-                    int nextRow = sharks[i].curPosY + dir[movePriority[i][sharks[i].curDir.ordinal() - 1][j].ordinal() - 1][0];
-                    int nextCol = sharks[i].curPosX + dir[movePriority[i][sharks[i].curDir.ordinal() - 1][j].ordinal() - 1][1];
-                    if (check(nextRow, nextCol) && board[nextRow][nextCol].owner == i) {
-                        sharks[i].curPosY = nextRow;
-                        sharks[i].curPosX = nextCol;
-                        sharks[i].curDir = movePriority[i][sharks[i].curDir.ordinal() - 1][j];
-                        break;
-                    }
-                }
+            // queue가 전부 빌 때까지 연결할 수 있는 노드가 발견되지 않은 경우
+            if (minDist == Integer.MAX_VALUE) return -1;
+            result += minDist;
+            selected[cur] = true;
+            // 새롭게 연결된 노드의 간선들을 모두 PQ에 add
+            for (int j = 0; j < graph.get(cur).size(); j++) {
+                dist.add(graph.get(cur).get(j));
             }
         }
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (board[i][j].life > 1) {
-                    board[i][j].life--;
-                } else if (board[i][j].life == 1) {
-                    board[i][j].life = 0;
-                    board[i][j].owner = 0;
-                }
-            }
-        }
-
-        for (int i = 1; i <= sharkNum; i++) {
-            if (!sharks[i].state)
-                continue;
-            if (board[sharks[i].curPosY][sharks[i].curPosX].owner == 0 ||
-                    board[sharks[i].curPosY][sharks[i].curPosX].owner == i) {
-                board[sharks[i].curPosY][sharks[i].curPosX].owner = i;
-                board[sharks[i].curPosY][sharks[i].curPosX].life = smellTime;
-            } else {
-                sharks[i].state = false;
-                remainShark--;
-            }
-        }
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        size = Integer.parseInt(st.nextToken());
-        board = new Smell[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                board[i][j] = new Smell(0, 0);
-            }
-        }
-        sharkNum = Integer.parseInt(st.nextToken());
-        movePriority = new DIRECTION[sharkNum + 1][4][4];
-        sharks = new Shark[sharkNum + 1];
-        remainShark = sharkNum;
-        smellTime = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < size; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < size; j++) {
-                int tmp = Integer.parseInt(st.nextToken());
-                if (tmp != 0) {
-                    sharks[tmp] = new Shark(true, DIRECTION.ZERO, i, j);
-                    board[i][j].life = smellTime;
-                    board[i][j].owner = tmp;
-                }
-            }
+        schoolNum = Integer.parseInt(st.nextToken());
+        int roadNum = Integer.parseInt(st.nextToken());
+        schoolType = new boolean[schoolNum + 1];
+        selected = new boolean[schoolNum + 1];
+        for (int i = 0; i < schoolNum + 1; i++) {
+            graph.add(new ArrayList<>());
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= sharkNum; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            sharks[i].curDir = numToDir(tmp);
-        }
-
-        for (int idx = 1; idx <= sharkNum; idx++) {
-            for (int priority = 0; priority < 4; priority++) {
-                st = new StringTokenizer(br.readLine());
-                for (int i = 0; i < 4; i++) {
-                    int tmp = Integer.parseInt(st.nextToken());
-                    movePriority[idx][priority][i] = numToDir(tmp);
-                }
+        for (int i = 1; i <= schoolNum; i++) {
+            if (st.nextToken().equals("M")) {
+                schoolType[i] = true;
+            } else {
+                schoolType[i] = false;
             }
         }
 
-        int time = 0;
-        while (time <= 1000) {
-            time++;
-            move();
-            if (remainShark == 1) {
-                break;
-            }
+        for (int i = 0; i < roadNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int length = Integer.parseInt(st.nextToken());
+            if (schoolType[from] == schoolType[to])
+                continue;
+            graph.get(from).add(new Pair(to, length));
+            graph.get(to).add(new Pair(from, length));
         }
 
-        if (time <= 1000)
-            System.out.println(time);
-        else
-            System.out.println(-1);
+        System.out.println(prim());
     }
 }
